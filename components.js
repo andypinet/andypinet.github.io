773f6db29fc280f61e3ae97cc30ab4458cf60efc
Andy <1585638808@qq.com>
2017-05-05 13:12:46 +0800

    今天是个好日子:information_desk_person:

--- components.js
+++ components.js
@@ -28,28 +28,30 @@ function getUnmaskedInfo(gl) {
 
 function findIP(onNewIP) { //  onNewIp - your listener function for new IPs
   var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection; //compatibility for firefox and chrome
-  var pc = new myPeerConnection({iceServers: []}),
-    noop = function() {},
-    localIPs = {},
-    ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
-    key;
+  if (myPeerConnection) {
+    var pc = new myPeerConnection({iceServers: []}),
+        noop = function() {},
+        localIPs = {},
+        ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+        key;
 
-  function ipIterate(ip) {
-    if (!localIPs[ip]) onNewIP(ip);
-    localIPs[ip] = true;
+    function ipIterate(ip) {
+        if (!localIPs[ip]) onNewIP(ip);
+        localIPs[ip] = true;
+    }
+    pc.createDataChannel(""); //create a bogus data channel
+    pc.createOffer(function(sdp) {
+        sdp.sdp.split('\n').forEach(function(line) {
+        if (line.indexOf('candidate') < 0) return;
+        line.match(ipRegex).forEach(ipIterate);
+        });
+        pc.setLocalDescription(sdp, noop, noop);
+    }, noop); // create offer and set local description
+    pc.onicecandidate = function(ice) { //listen for candidate events
+        if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+        ice.candidate.candidate.match(ipRegex).forEach(ipIterate);
+    };
   }
-  pc.createDataChannel(""); //create a bogus data channel
-  pc.createOffer(function(sdp) {
-    sdp.sdp.split('\n').forEach(function(line) {
-      if (line.indexOf('candidate') < 0) return;
-      line.match(ipRegex).forEach(ipIterate);
-    });
-    pc.setLocalDescription(sdp, noop, noop);
-  }, noop); // create offer and set local description
-  pc.onicecandidate = function(ice) { //listen for candidate events
-    if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
-    ice.candidate.candidate.match(ipRegex).forEach(ipIterate);
-  };
 }
 
 
@@ -99,13 +101,13 @@ Vue.component("andy-yourdeviceinfo", {
                  value: getUnmaskedInfo(gl).renderer
              }                                                                                     
         ];
-        // findIP(function (newIps) {
-        //     ret.items.push({
-        //         key: "IP",
-        //         value: newIps
-        //     });
-        //     console.log('got ip: ', newIps);            
-        // });
+        findIP(function (newIps) {
+            ret.items.push({
+                key: "IP",
+                value: newIps
+            });
+            console.log('got ip: ', newIps);            
+        });
         return ret;
     }
 });
