a42a5946316f54e66574628065b23110c5eed641
Andy <1585638808@qq.com>
2017-05-05 09:57:43 +0800

    回滚版本到分支中的某一个commit时间点 git reset [commit 哈希值]:shaved_ice:

--- app.js
+++ app.js
@@ -93,10 +93,39 @@ var loadCSS = function(url, callback){
         img.src = url;
 }
 
+loadScript = function(url) {
+    return new Promise(function(resolve, reject) {
+        // Adding the script tag to the head as suggested before
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = url + "?v=" + new Date();
+
+        // Then bind the event to the callback function.
+        // There are several events for cross browser compatibility.
+        script.onreadystatechange = function() {
+            resolve();
+        };        
+        script.onload = function() {
+            resolve();
+        };
+        script.onerror = function(e) {
+            reject(e);
+        }
+
+        // Fire the loading
+        head.appendChild(script);        
+    })
+}
+
 var app = new Vue({
   el: '#app',
   template: '#apptemplate',
   data: function () {
+      window.client = new ClientJS();       
+      if (window.client.isMobile()) {
+        loadScript("./vconsole.min.js");
+      }
       return {
           global: {
               now: new Date().toLocaleTimeString()
